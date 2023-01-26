8f50976bd73596281388859c484ebb14206dc12d
Aelfric <aelfric.mayer@gmx.at>
2023-01-26 18:03:10 +0100

    Initial commit

---
+++ src/test/java/com/example/slmpruefungmayer/SlMpruefungMayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmpruefungmayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlMpruefungMayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
